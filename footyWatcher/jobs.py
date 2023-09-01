6516c5cf137747ad747c01693b96862f321a4d0c
Diogo Gonçalves <rxkax@hotmail.com>
2023-09-01 14:51:27 +0100

    Added logic to get list of ghames

--- footyWatcher/jobs.py
+++ footyWatcher/jobs.py
@@ -7,17 +7,25 @@ from footyWatcher.models import Competition
 from django.shortcuts import get_object_or_404
 
 URL = "https://soccerlive.app/"
-COMPETITION_TYPE= "div"
-COMPETITION_CLASS= "top-tournament"
-COMPETITION_TITLE_TYPE= "span"
-COMPETITION_TITLE_CLASS= "league-name"
+COMPETITION_TYPE = "div"
+COMPETITION_CLASS = "top-tournament"
+COMPETITION_TITLE_TYPE = "span"
+COMPETITION_TITLE_CLASS = "league-name"
+GAME_LINK_TYPE = "a"
+GAME_LINK_CLASS = "competition"
+
+TEAMS_TYPE = "span"
+HOME_TEAM_CLASS = "competition-cell-side1"
+AWAY_TEAM_CLASS = "competition-cell-side2"
+
 
 class CheckJob(threading.Thread):
     def run(cls):
         while True:
             run_pending()
             time.sleep(1)
-    
+
+
 @repeat(every(30).minutes)
 def update_games():
     print("Starting job to update games")
@@ -29,18 +37,31 @@ def update_games():
         handle_competition(competion)
         print()
 
+
 def handle_competition(competition_element):
-    competition_name_element = competition_element.find(COMPETITION_TITLE_TYPE, class_=COMPETITION_TITLE_CLASS)
-    if(competition_name_element != None):
+    competition_name_element = competition_element.find(
+        COMPETITION_TITLE_TYPE, class_=COMPETITION_TITLE_CLASS
+    )
+    if competition_name_element != None:
         competition_name = competition_name_element.text.strip()
         try:
             competition = Competition.objects.get(name=competition_name)
-            print("Found competition")
+            print(f"Found competition {competition.name}")
         except Competition.DoesNotExist:
-            competition = Competition(name= competition_name_element.text.strip())
+            competition = Competition(name=competition_name_element.text.strip())
             competition.save()
-            print("Created competition")
-        print(competition)
+            print(f"Created competition {competition.name}")
+    for game in competition_element.find(GAME_LINK_TYPE, class_=GAME_LINK_CLASS):
+        print(game)
+        # handle_game(game, competition)
+
+
+def handle_game(game, competition):
+    home_team_element = game.find(TEAMS_TYPE, class_=HOME_TEAM_CLASS)
+    print(home_team_element)
+    away_team_element = game.find(TEAMS_TYPE, class_=AWAY_TEAM_CLASS)
+    print(away_team_element)
+
 
 def start():
     update_games()
