5549dfba1e955fee3e3da136f1ec63b906bf283c
Diogo Gonçalves <dgoncalves@agap2.pt>
2023-09-11 10:39:59 +0100

    Added logic to get teams names

--- footyWatcher/jobs.py
+++ footyWatcher/jobs.py
@@ -1,10 +1,10 @@
 from schedule import every, repeat, run_pending
 import time
+from django.utils import timezone
 import threading
 import requests
 from bs4 import BeautifulSoup
-from footyWatcher.models import Competition
-from django.shortcuts import get_object_or_404
+from footyWatcher.models import Competition, Game
 
 URL = "https://soccerlive.app/"
 COMPETITION_TYPE = "div"
@@ -15,8 +15,13 @@ GAME_LINK_TYPE = "a"
 GAME_LINK_CLASS = "competition"
 
 TEAMS_TYPE = "span"
+TEAM_NAME_CLASS = "name"
 HOME_TEAM_CLASS = "competition-cell-side1"
 AWAY_TEAM_CLASS = "competition-cell-side2"
+GAME_TIME_CLASS = "competition-cell-score"
+TIME_CLASS = "time"
+TIME_VARIABLE = "datetime"
+GAME_URL_VARIABLE = "href"
 
 
 class CheckJob(threading.Thread):
@@ -51,16 +56,48 @@ def handle_competition(competition_element):
             competition = Competition(name=competition_name_element.text.strip())
             competition.save()
             print(f"Created competition {competition.name}")
-    for game in competition_element.find(GAME_LINK_TYPE, class_=GAME_LINK_CLASS):
-        print(game)
-        # handle_game(game, competition)
+        print(competition.game_set.all())
+        for game in competition_element.find_all(
+            GAME_LINK_TYPE, class_=GAME_LINK_CLASS
+        ):
+            try:
+                handle_game(game, competition)
+            except:
+                print("Could not create game")
 
 
 def handle_game(game, competition):
-    home_team_element = game.find(TEAMS_TYPE, class_=HOME_TEAM_CLASS)
-    print(home_team_element)
-    away_team_element = game.find(TEAMS_TYPE, class_=AWAY_TEAM_CLASS)
-    print(away_team_element)
+    game_link = game["href"]
+    game_home_team = (
+        game.find(TEAMS_TYPE, class_=HOME_TEAM_CLASS)
+        .find(TEAMS_TYPE, class_=TEAM_NAME_CLASS)
+        .text.strip()
+    )
+    game_away_team = (
+        game.find(TEAMS_TYPE, class_=AWAY_TEAM_CLASS)
+        .find(TEAMS_TYPE, class_=TEAM_NAME_CLASS)
+        .text.strip()
+    )
+    try:
+        game = competition.game_set.filter(
+            home_team=game_home_team, away_team=game_away_team
+        ).get()
+        print(f"Found game {game.home_team} vs {game.away_team}")
+    except Game.DoesNotExist:
+        game = competition.game_set.create(
+            home_team=game_home_team,
+            away_team=game_away_team,
+            start_date=get_game_date(game),
+        )
+
+
+def get_game_date(game):
+    game_start_date_element = game.find(TEAMS_TYPE, class_=GAME_TIME_CLASS)
+    time_element = game_start_date_element.find(TIME_CLASS)
+    if time_element != None:
+        return time_element[TIME_VARIABLE]
+    else:
+        return timezone.now()
 
 
 def start():
